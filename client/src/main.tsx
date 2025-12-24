83231a13e3e7feb145dc36175e1685d8d2e46217
Arwa Mahmoud <arwamah777@gmail.com>
2025-12-24 18:45:04 +0200

    Add Vercel Speed Insights to client app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,19 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { AuthProvider } from './contexts/AuthContext';
-import React from 'react';
+import { AuthProvider } from "./contexts/AuthContext";
+import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // Remove loading class once app is ready
 const root = document.getElementById("root")!;
 root.classList.remove("app-loading");
 
 createRoot(root).render(
-    <React.StrictMode>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+      <SpeedInsights />
+    </AuthProvider>
+  </React.StrictMode>
 );
